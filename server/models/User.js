12d3c8f8f54417254b4bd8ccdb48ec66f3d00b35
MASIRAPA VIJAY VARDHAN <164061130+vijay3063@users.noreply.github.com>
2025-07-07 01:26:04 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     _id: {type:String, required: true},
     name: {type:String, required: true},
     email: {type:String, required: true, unique: true},
-    resume: {tyep:String},
+    resume: {type:String},
     image: {type:String, required: true},
 })
 
